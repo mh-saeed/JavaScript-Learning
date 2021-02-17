1cce31a7d42009fa20cde4933877b483a5d615ab
mh-saeed <saeedkhan111244@gmail.com>
2021-02-17 12:51:43 +0500

    updated

--- tut7.js
+++ tut7.js
@@ -1,12 +1,73 @@
 // Arrays
 
-const marks = [67, 88, 98, 74, 69, 96];
+let marks = [67, 88, 98, 74, 69, 96];
 const fruits = [`Apple`, `Mango`, `Banana`, `Orange`, `Pineapple`];
 const mixed = [`str`, 32, [23, 12]];
 
 const arr = new Array(43, 123, 125, `Hassan`, `Saeed`, `Moiz`);
 
 console.log(arr);
+arr[0]=`Saeed`;
+console.log(arr);
+
 console.log(marks);
 console.log(fruits);
 console.log(mixed);
+
+x=0;
+while (x<mixed.length) {
+    console.log(mixed[x]);
+    x++;
+}
+
+console.log(arr.length); // array methods(with braces) and properties(without braces). lenghth is property
+console.log(Array.isArray(arr)); // array methods(with braces) and properties(without braces). lenghth is property
+
+let findLocatonOf = marks.indexOf(74);
+console.log(findLocatonOf); // will return index of given element
+
+// Mutating or Modifying
+
+marks.push(99); // will add element at the end(last index) of an array
+console.log(marks);
+
+marks.unshift(0900); // will add at 0 index of an array
+console.log(marks);
+
+marks.pop(); // will remove element from the last index of an array
+console.log(marks);
+
+marks.shift(); // will remove from 0 index of an array
+console.log(marks);
+
+marks.splice(1,4); // will remove elements splice(from index, number of elements to be removed)
+console.log(marks);
+
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+let marks2 = [25, 55,84,124];
+marks = marks.concat(marks2);
+console.log(marks);
+
+let myInfo = {
+    name:`Muhammad Saeed`,
+    age: 21 ,
+    uniName: `MUET`,
+    rollNo: 123,
+    marks1: [22,334,455],
+    deptName: `Software Eng:`
+    
+};
+console.log(myInfo);
+console.log(myInfo.name);
+
+console.log(myInfo[`name`]);
+console.log(myInfo.uniName);
+
+console.log(myInfo.marks1);
+console.log(myInfo.marks1[0]);
+
+
+
---
+++ tut8.js
@@ -0,0 +1,62 @@
+console.log(`Tutorial 8`);
+
+const age = 20;
+
+let myAge = 21;
+
+
+// double == will only check/compare values, while triple === will check/compare both values & type of data.
+// != value not equal to, !== value & type not equal to
+
+if (age === `20`) {
+    console.log(`age is 20 😠, type: string`);
+}
+else if (age == 19 ) {
+    console.log(`age is 19 😂`);
+}
+else if (age === 20 ) {
+    console.log(`age is 20 😍, type: number`);
+}
+else{
+    console.log(`age is neither 20 nor 19 😢`);
+}
+
+if (typeof myAge !== `undefined`) {
+    console.log(`myAge is defined!`);    
+} else {
+    console.log(`myAge is not defined!`);        
+}
+
+let doesDrive = true;
+
+
+if (myAge<18) {
+    console.log(`You are under-Age 😢`);     
+}
+else if (doesDrive || myAge<18) {
+    console.log(`You can't Drive 😠`);    
+    
+}
+else if (doesDrive && myAge>=18) {
+    console.log(`You can Drive 🙂`);    
+}
+
+// ternary operator
+console.log(myAge==21? `my age is 21`:`my age is not 21`)//myAge==='21'? `my age is 21`:`my age is not 21`
+// (condition? if true display this: if false display this)
+
+//switch
+
+switch (myAge) {
+    case 18:
+        console.log(`You can't Drive 😠`);    
+        break;
+    case 17:
+    console.log(`You are under-Age 😢`);     
+        break;
+
+    default:
+    console.log(`You can't Drive 😠`);    
+
+        break;
+}
