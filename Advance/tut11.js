693ab99814ff5f7abf5bea564b2f930244d29971
mh-saeed <saeedkhan111244@gmail.com>
2021-02-23 21:28:06 +0500

    updated

---
+++ Advance/tut11.js
@@ -0,0 +1,41 @@
+// DOM (Document Object Model)
+
+let a = window;
+
+// window.alert(`Hello Saeed`);
+
+/* let mess = prompt(`Enter your Name`);
+console.log(`Your name is ` + mess);
+ */
+
+/* let conf = confirm(`Are you 18+?`); // will give true if user click on ok and false if user click on cancel
+console.log(conf); */ //will give boolean value
+
+console.log(a);
+
+let d = window.document; 
+console.log(d);
+
+console.log(window.innerHeight); // will print height of window 
+console.log(window.innerWidth); // will print Width of window 
+
+console.log(window.scrollX); 
+console.log(window.scrollY);  
+
+console.log(window.location);
+console.log(`\n`, location.href);
+console.log(`\n`, location.toString());
+
+
+console.log(`\n`, window.history);
+console.log(`\n`, location.toString());
+
+// history.back() // one step backward
+// history.forward() // on step forward
+//history.go(1) // positve value to go forward and negative to go backward
+//location.reload(); // will reload the page
+
+
+
+
+
