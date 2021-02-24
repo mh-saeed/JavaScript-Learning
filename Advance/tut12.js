eef39ff4ae52c7ebaac04c40dc64044578d045f1
mh-saeed <saeedkhan111244@gmail.com>
2021-02-25 01:43:51 +0500

    updated!

--- Advance/tut12.js
+++ Advance/tut12.js
@@ -1,11 +1,28 @@
 // DOM and Website layout
 console.log(`Tutorial 12`);
 
-a = document; // will give complete html document
+/* a = document; // will give complete html document
 a = document.all; // will give all html tags in collection
 a = document.body; // will give all body of html in collection
 a = document.forms; // will give all forms of html in collection
 a = document.links; // will give all links of html in collection
+a = document.forms.length; // will give length/number of forms in collection
+a = document.forms[0]; // will give forms of specific index in collection
+// a = document.forms.length; // will give length/number of forms in collection
+a = document.links.length; // will give all links of html in collection
+a = document.links[1]; // will give all links of html in collection
+
+a = document.all; // will give all html tags in collection
+a = document.images; // will give all html tags in collection
+a = document.scripts; // will give all html tags in collection */
 
 
-console.log(a);
+/* Array.from(a).forEach(function (element) {
+    console.log(element);
+}); */
+
+// console.log(a);
+
+
+
+
