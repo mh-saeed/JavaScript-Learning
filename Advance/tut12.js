8d75b00d3848011f9e556cd023abdf09dae1b2e5
mh-saeed <saeedkhan111244@gmail.com>
2021-02-25 00:24:08 +0500

    Create tut12.js

---
+++ Advance/tut12.js
@@ -0,0 +1,11 @@
+// DOM and Website layout
+console.log(`Tutorial 12`);
+
+a = document; // will give complete html document
+a = document.all; // will give all html tags in collection
+a = document.body; // will give all body of html in collection
+a = document.forms; // will give all forms of html in collection
+a = document.links; // will give all links of html in collection
+
+
+console.log(a);
