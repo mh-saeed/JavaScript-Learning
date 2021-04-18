0fd772e95468a26f915945fbb6d9c98d596b023b
mh-saeed <saeedkhan111244@gmail.com>
2021-04-18 16:38:43 +0500

    done

--- Advance/tut17_Events&EventsObject.js
+++ Advance/tut17_Events&EventsObject.js
@@ -1,12 +1,31 @@
 let elem = document.getElementById(`search`);
 
 elem.addEventListener(`click`, function (e) {
-  elem.style.color = `Green`;
-  let x = document.getElementById("myText");
-  var defaultVal = x.defaultValue;
-  var currentVal = x.value;
-  location.href = `${currentVal ? currentVal : `https://${defaultVal}`}`;
 
-  let clicked = e.target;
-  console.log(clicked);
+    elem.style.color = `Green`;
+    let x = document.getElementById("myText");
+    var defaultVal = x.defaultValue;
+    var currentVal = x.value;
+
+    defaultVal == currentVal
+        ? (location.href = defaultVal)
+        : (location.href = currentVal);
+});
+
+let elem2 = document.getElementById(`heading`);
+
+elem2.addEventListener(`click`, function (e) {
+
+    let clicked = e.target;
+    clicked = e.target.className; // will give class name
+    clicked = e.target.classList; // will give all classes names
+    clicked = e.target.id; // will give id
+
+    clicked = e.offsetX; // will print loction of clicked area
+    clicked = e.offsetY; // will print loction of clicked area
+    clicked = e.clientX; // will print loction of clicked area
+    clicked = e.clientY; // will print loction of clicked area
+
+    console.log(`CLICKED`);
+    console.log(clicked);
 });
