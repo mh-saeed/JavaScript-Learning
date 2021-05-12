6396fbed6a60e9992c1241fd973135a77ed726eb
mh-saeed <saeedkhan111244@gmail.com>
2021-05-13 00:18:34 +0500

    updated

--- Advance/tut28_ObjectPrototype.js
+++ Advance/tut28_ObjectPrototype.js
@@ -1,27 +1,38 @@
-// Object literal
-let obj1 = {
-  name: `Muhammad Saeed`,
+// --------------------------------Object literal--------------------------------
+let uni = {
   uni: `MUET`,
+  location: `Jamshoro`,
+};
+let std = {
+  name: `Muhammad Saeed`,
+  rollNo: `18sw123`,
   dept: `Software`,
 };
-console.log(obj1);
 
-//Object prototype
+std.__proto__ = uni; // sets std to be a prototype of uni
 
-function Obj2(giveName, rollNo, dept ) {
+console.log(std);
+console.log(std.uni + `,   ` + std.location);
+
+//--------------------------------Object prototype--------------------------------
+
+// creating object contructor
+function Obj2(giveName, rollNo, dept) {
   this.name = giveName;
   this.rollNo = rollNo;
-  this.dept=dept;
-
+  this.dept = dept;
 }
+
+// setters
 Obj2.prototype.getName = function () {
   return this.name;
 };
+// getters
+Obj2.prototype.setName = function (setName) {
+  this.name = setName;
+};
 
-Obj2.prototype.setName = function(setName) {
-    this.name= setName;
-}
-let std1 = new Obj2(`Muhammad Saeed`,`18sw123`,`software`);
+let std1 = new Obj2(`Muhammad Saeed`, `18sw123`, `software`);
 
 console.log(std1);
 console.log(std1.getName());
