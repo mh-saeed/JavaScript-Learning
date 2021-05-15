80880b9e8a38bbeec51ba4dbac5bfdfb83dfdf85
mh-saeed <saeedkhan111244@gmail.com>
2021-05-16 00:01:11 +0500

    updated

--- Basic/tut8_IfElse&Switches.js
+++ Basic/tut8_IfElse&Switches.js
@@ -33,10 +33,11 @@ if (myAge < 18) {
   console.log(`You can Drive 🙂`);
 }
 
-// ternary operator
+// -----------------------------------ternary operator-----------------------------------
+
 console.log(myAge == 21 ? `my age is 21` : `my age is not 21`); //myAge==='21'? `my age is 21`:`my age is not 21`
 
-//Multiple ‘?’
+//-----------------------------------Multiple----------------------------------- ‘?’
 
 // A sequence of question mark operators ? can return a value that depends on more than one condition.
 
@@ -78,3 +79,54 @@ switch (myAge) {
 
     break;
 }
+//----------------------Breaking the loop----------------------
+
+//For example, the loop below asks the user for a series of numbers, “breaking” when no number is entered:
+let sum = 0;
+while (true) {
+  let value = +prompt("Enter a number", "");
+  if (!value) break; // (*)
+  sum += value;
+}
+alert("Sum: " + sum);
+
+//The break directive is activated at the line (*) if the user enters an empty line or cancels the
+//input. It stops the loop immediately, passing control to the first line after the loop. Namely,
+//alert .
+
+//----------------------Continue to the next iteration----------------------
+
+// The continue directive is a “lighter version” of break . It doesn’t stop the whole loop.
+// Instead, it stops the current iteration and forces the loop to start a new one (if the condition
+// allows).
+
+for (let i = 0; i < 10; i++) {
+  // if true, skip the remaining part of the body
+  if (i % 2 == 0) continue;
+  alert(i); // 1, then 3, 5, 7, 9
+}
+
+// Please note that syntax constructs that are not expressions cannot be used with the ternary
+// operator ? . In particular, directives such as break/continue aren’t allowed there.
+
+//----------------------Labels for break/continue----------------------
+
+outer: for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    let input = prompt(`Value at coords (${i},${j})`, "");
+    // if an empty string or canceled, then break out of both loops
+    if (!input) break outer; // (*)
+    // do something with the value...
+  }
+}
+alert("Done!");
+
+/* In the code above, break outer looks upwards for the label named outer and breaks out
+of that loop.
+So the control goes straight from (*) to alert('Done!') .
+We can also move the label onto a separate line: 
+
+//////         Note: 
+A call to break/continue is only possible from inside a loop and the label must be
+somewhere above the directive.
+*/
