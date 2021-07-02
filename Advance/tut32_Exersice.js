07e2bf0caf8bb9279d639e959be1176e613fd853
mh-saeed <saeedkhan111244@gmail.com>
2021-07-02 19:13:14 +0500

    Update tut32_Exersice.js

--- Advance/tut32_Exersice.js
+++ Advance/tut32_Exersice.js
@@ -2,6 +2,6 @@
 
 1. Create a constructor that takes the book list as an argument.
 2. getBookList(): This function should return the list of all book present in the library.
-3. issueBook(bookname, user): This function takes an argument of bookname and user, in bookname, we specify the name of the book, and in user, we specify the name of the user. This function will issue the book to the user.
-4.returnBook(bookname): This function will take one argument, which is the name of the book. The purpose of this function is to remove the book from the users borrowed book data. It is totally up to you how you implement it.
+3. issueBook(bookName, user): This function takes an argument of bookName and user, in bookName, we specify the name of the book, and in user, we specify the name of the user. This function will issue the book to the user.
+4.returnBook(bookName): This function will take one argument, which is the name of the book. The purpose of this function is to remove the book from the users borrowed book data. It is totally up to you how you implement it.
  */
