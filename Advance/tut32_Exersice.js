d9016602cf54aa2b8479768d8af1746c489e41fe
mh-saeed <saeedkhan111244@gmail.com>
2021-07-29 00:22:37 +0500

    tut 32 added

--- Advance/tut32_Exersice.js
+++ Advance/tut32_Exersice.js
@@ -5,3 +5,36 @@
 3. issueBook(bookName, user): This function takes an argument of bookName and user, in bookName, we specify the name of the book, and in user, we specify the name of the user. This function will issue the book to the user.
 4.returnBook(bookName): This function will take one argument, which is the name of the book. The purpose of this function is to remove the book from the users borrowed book data. It is totally up to you how you implement it.
  */
+
+class Library {
+  constructor(bookList) {
+    this.bookList = bookList;
+    this.issuedBooks = {};
+  }
+
+  getBookList() {
+    this.bookList.forEach((element) => {
+      console.log(element);
+    });
+  }
+
+  issueBook(bookName, user) {
+    if (this.issuedBooks[bookName] == undefined) {
+      this.issuedBooks[bookName] = user;
+    }
+    else{
+        console.log("book is already issued")
+    }
+  }
+
+  returnBook(bookName) {
+      delete this.issuedBooks[bookName];
+  }
+
+}
+library1 = new Library(["C++","Java","Python","JavaScript"]);
+library1.issueBook("C++","saeed");
+library1.issueBook("java","harry");
+// library1.issueBook("java","harks"); will give message that book is already issued
+
+
